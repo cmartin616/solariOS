8d5d6437525057bb50ecd32136f2e199de875887
Chris Martin <chrismartin@x-134-84-56-134.uofm-secure.wireless.umn.edu>
2014-09-24 17:59:27 -0500

    Initial Commit

---
+++ solarP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  solarP
+//
+//  Created by Chris Martin on 9/24/14.
+//  Copyright (c) 2014 Chris Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ solarP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  solarP
+//
+//  Created by Chris Martin on 9/24/14.
+//  Copyright (c) 2014 Chris Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

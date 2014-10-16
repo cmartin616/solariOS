fc160c8c3b6f49815a28537a4d466b9ae8612ad4
Chris Martin <chrismartin@Administrators-MacBook-Pro.local>
2014-10-16 13:41:44 -0500

    Initial Commit

--- solarP/ViewController.h
+++ solarP/ViewController.h
@@ -9,7 +9,65 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *titleText;
 
+// BACKGROUND
+@property (weak, nonatomic) IBOutlet UIImageView *solarView;
+@property (weak, nonatomic) IBOutlet UIImageView *streetView;
+@property (weak, nonatomic) IBOutlet UIImageView *aerialView;
+
+
+// MAIN LAYOUT
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UILabel *solarPIcon;
+
+@property (weak, nonatomic) IBOutlet UILabel *resultsLabel;
+@property (weak, nonatomic) IBOutlet UIButton *resultsTab;
+@property (weak, nonatomic) IBOutlet UIView *resultsPage;
+- (IBAction)openResultsPage:(id)sender;
+
+// BOTTOM BAR
+
+@property (weak, nonatomic) IBOutlet UIImageView *bottomBar;
+@property (weak, nonatomic) IBOutlet UIButton *resultsButton;
+- (IBAction)toggleResults:(id)sender;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *streetsButton;
+- (IBAction)toggleStreets:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *aerialButton;
+- (IBAction)toggleAerial:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *solarButton;
+- (IBAction)toggleSolar:(id)sender;
+
+// SIDE NAV
+
+@property (weak, nonatomic) IBOutlet UILabel *helpLabel;
+@property (weak, nonatomic) IBOutlet UIButton *helpTab;
+@property (weak, nonatomic) IBOutlet UIView *helpPage;
+- (IBAction)openHelpPage:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *bookmarkLabel;
+@property (weak, nonatomic) IBOutlet UIButton *bookmarkTab;
+@property (weak, nonatomic) IBOutlet UIView *bookmarkPage;
+- (IBAction)openBookmarkPage:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *menuLabel;
+@property (weak, nonatomic) IBOutlet UIButton *menuTab;
+@property (weak, nonatomic) IBOutlet UIView *menuPage;
+- (IBAction)openMenuPage:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIView *histogramPage;
+@property (weak, nonatomic) IBOutlet UIButton *histogramLowerTab;
+- (IBAction)openHistogramPage:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *resultsLowerTab;
+
+@property (weak, nonatomic) IBOutlet UIView *savePage;
+@property (weak, nonatomic) IBOutlet UIButton *saveLowerTab;
+- (IBAction)openSavePage:(id)sender;
 
 @end
 
--- solarP/ViewController.m
+++ solarP/ViewController.m
@@ -17,11 +17,144 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Loaded properly");
+    //self.splashScreenBackground.hidden = isHidden;
+    self.bookmarkPage.hidden = YES;
+    self.menuPage.hidden = YES;
+    self.helpPage.hidden = YES;
+    self.resultsPage.hidden = YES;
+    //self.titleText.text = @"Solar Suitability";
+    self.solarPIcon.text= @"SolarP";
+    //[self createSplashScreen];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)closeSideBar{
+    self.resultsPage.hidden = YES;
+    self.histogramPage.hidden = YES;
+    self.savePage.hidden = YES;
+}
+
+/*- (void)createSplashScreen{
+    isHidden = NO;
+    self.splashScreenBackground.hidden = isHidden;
+    NSLog(@"Loaded Splash");
+    self.splashScreenText.text = @"Splash Screen";
+    self.splashScreenBodyText.text= @"WELCOME\
+    \
+    Tap anywhere on this map of Minnesota to view solar radiation per square meter, or tap the  icon to measure insolation at your current location.";
+}*/
+
+int mapShow=0;
+
+- (IBAction)openBookmarkPage:(id)sender {
+    if (self.bookmarkPage.hidden == YES){
+        NSLog(@"BOOK HIDDEN");
+        self.menuPage.hidden = YES;
+        self.bookmarkPage.hidden = NO;
+    }else{
+        self.bookmarkPage.hidden = YES;
+    }
+}
+- (IBAction)openMenuPage:(id)sender {
+    if (self.menuPage.hidden == YES){
+        NSLog(@"MENU HIDDEN");
+        self.bookmarkPage.hidden = YES;
+        self.menuPage.hidden = NO;
+    }else{
+        self.menuPage.hidden = YES;
+    }
+}
+- (IBAction)openResultsPage:(id)sender {
+    if (self.resultsPage.hidden == YES){
+        NSLog(@"MENU HIDDEN");
+        self.helpPage.hidden = YES;
+        self.resultsPage.hidden = NO;
+    }else{
+        self.resultsPage.hidden = YES;
+    }
+}
+- (IBAction)openHelpPage:(id)sender {
+    if (self.helpPage.hidden == YES){
+        NSLog(@"MENU HIDDEN");
+        self.resultsPage.hidden = YES;
+        self.helpPage.hidden = NO;
+    }else{
+        self.helpPage.hidden = YES;
+    }
+}
+- (IBAction)openHistogramPage:(id)sender {
+    [self closeSideBar];
+    self.histogramPage.hidden = NO;
+}
+
+- (IBAction)openSavePage:(id)sender {
+    [self closeSideBar];
+    self.savePage.hidden = NO;
+    
+}
+- (IBAction)toggleSolar:(id)sender {
+    NSLog(@"SOLAR CLICK");
+    NSLog(@"Map1 clicked");
+    mapShow=1;
+    [self updateDisplay];
+    /*if (self.solarView.hidden == YES){
+        self.solarView.hidden = NO;
+        self.aerialView.hidden = YES;
+        self.streetView.hidden = YES;
+    }*/
+}
+- (IBAction)toggleAerial:(id)sender {
+    NSLog(@"AERIAL CLICK");
+    NSLog(@"Map2 clicked");
+    mapShow=2;
+    [self updateDisplay];
+    
+    /*if (self.aerialView.hidden == YES){
+        self.aerialView.hidden = NO;
+        self.solarView.hidden = YES;
+        self.streetView.hidden = YES;
+    }*/
+}
+
+- (IBAction)toggleStreets:(id)sender {
+    NSLog(@"STREETS CLICK");
+    NSLog(@"Map3 clicked");
+    mapShow=3;
+    [self updateDisplay];
+    
+    /*NSLog(@"SOLAR CLICK");
+    if (self.streetView.hidden == YES){
+        self.streetView.hidden = NO;
+        self.aerialView.hidden = YES;
+        self.solarView.hidden = YES;
+    }*/
+}
+- (IBAction)toggleResults:(id)sender {
+    NSLog(@"RESULTS CLICK");
+}
+
+- (void)updateDisplay{
+    
+    switch (mapShow) {
+        case 1:
+            self.solarView.image = [UIImage imageNamed:@"map_background.tiff"];
+            break;
+        case 2:
+            self.solarView.image = [UIImage imageNamed:@"redXCircle.jpg"];
+            break;
+        case 3:
+            self.solarView.image = [UIImage imageNamed:@"redXCircle.jpg"];
+            break;
+        default:
+            self.solarView.image = [UIImage imageNamed:@"map_background.tiff"];
+            break;
+    }
 }
 
 @end

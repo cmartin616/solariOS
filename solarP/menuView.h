81471bf544ef93a17b113d66bfb70cb8c79baf52
Chris Martin <cmartin616@gmail.com>
2014-10-25 17:01:26 -0400

    Created ViewController.h and .m as well as menuView.h and .m.  Assigned classes to each view.

---
+++ solarP/menuView.h
@@ -0,0 +1,13 @@
+//
+//  menuView.h
+//  solarP
+//
+//  Created by Chris Martin on 10/25/14.
+//  Copyright (c) 2014 solarP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface menuView : UIViewController
+
+@end
---
+++ solarP/solarP/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  solarP
+//
+//  Created by Chris Martin on 10/25/14.
+//  Copyright (c) 2014 solarP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

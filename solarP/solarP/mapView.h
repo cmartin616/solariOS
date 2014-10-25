48cec5e82669d6ef2f7538532efc2c7f1a22611c
Chris Martin <cmartin616@gmail.com>
2014-10-25 17:41:20 -0400

    Added mapView so I could recover lost code from previous project as well as branch into arcGIS testing.

---
+++ solarP/solarP/mapView.h
@@ -0,0 +1,13 @@
+//
+//  mapView.h
+//  solarP
+//
+//  Created by Chris Martin on 10/25/14.
+//  Copyright (c) 2014 solarP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mapView : UIViewController
+
+@end
---
+++ solarP/solarP/mapView.m
@@ -0,0 +1,37 @@
+//
+//  mapView.m
+//  solarP
+//
+//  Created by Chris Martin on 10/25/14.
+//  Copyright (c) 2014 solarP. All rights reserved.
+//
+
+#import "mapView.h"
+
+@interface mapView ()
+
+@end
+
+@implementation mapView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
